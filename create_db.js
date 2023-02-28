cbe8d6eb662c997a058e6f9c8148741958f1f07b
Norman Alié <mail@normanalie.fr>
2023-02-28 16:45:45 +0100

    Migrate mongoose

--- create_db.js
+++ create_db.js
@@ -1,22 +1,20 @@
 require("dotenv").config();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");   
+const mongoose = require("mongoose");   
 
 const user = encodeURIComponent(process.env.DB_USER);
 const pass = encodeURIComponent(process.env.DB_PASS);                                                                           
 const url = `mongodb+srv://${user}:${pass}@cluster0.apwyjet.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 async function run() {
     try {
       console.log(`[MONGODB] - Try connection to ${url}`);
-      await client.connect();
+      await mongoose.connect(url);
       console.log("[MONGODB] - Connected.");
     } catch(err) {
         console.log(err.stack);
     } finally {
-      await client.close();
+      await mongoose.connection.close();
     }
   }
 
