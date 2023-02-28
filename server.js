007edffbec8aeaffec9defbfa2db56644903712a
Norman Alié <mail@normanalie.fr>
2023-02-28 23:11:53 +0100

    Add post

--- server.js
+++ server.js
@@ -8,12 +8,23 @@ const mongoose = require("mongoose");
 const db_connect = require("./create_db");
 const Image = require("./models/image");
 
+const bodyParser = require("body-parser");
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/api/images", (req, res)=>{
     Image.find()
         .then(image => res.status(200).json(image))
         .catch(err => res.status(400).json({err}));
+})
+
+app.post("/api/image", (req, res) => {
+    const image = new Image({ ...req.body });
+    image.save()
+        .then(() => res.status(201).json("Ok"))
+        .catch((err) => res.status(400).json({err}));
 })
 
 app.get("/api/tags", (req, res)=>{
